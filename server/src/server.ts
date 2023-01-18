82f6e74434fd74b77e6b056f2420bba2e565bb13
MarcelloBB <marcellobelanda@gmail.com>
2023-01-17 22:21:22 -0300

    Finish second class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,12 @@
 import Fastify from "fastify"
-import { PrismaClient } from "@prisma/client"
 import cors from "@fastify/cors"
-
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
+// Middlewares
 app.register(cors)
-
-app.get("/hello", async () => {
-  const habits = await prisma.habits.findMany({
-    where: {
-      title: {
-        startsWith: "Beber"
-      }
-    }
-  })
-
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333
